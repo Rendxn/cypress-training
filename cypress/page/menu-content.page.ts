34d7268d39516572dc4ba35cffa2ec5e963eacc9
Santiago Rendon <52801967+Rendxn@users.noreply.github.com>
2022-07-18 15:29:50 -0500

    test: added dresses list test

--- cypress/page/menu-content.page.ts
+++ cypress/page/menu-content.page.ts
@@ -1,9 +1,11 @@
 class MenuContentPage {
   private tShirtMenu: string;
+  private dressesMenu: string;
   private menuContentPageURL: string;
 
   constructor() {
     this.tShirtMenu = "li:nth-child(3) > a[title='T-shirts']";
+    this.dressesMenu = ".sf-menu > li:nth-child(2) > a[title='Dresses']";
     this.menuContentPageURL = "http://automationpractice.com/";
   }
 
@@ -14,6 +16,10 @@ class MenuContentPage {
   public goToTShirtMenu(): void {
     cy.get(this.tShirtMenu).click();
   }
+
+  public goToDressesMenu(): void {
+    cy.get(this.dressesMenu).click();
+  }
 }
 
-export {MenuContentPage};
+export { MenuContentPage };
