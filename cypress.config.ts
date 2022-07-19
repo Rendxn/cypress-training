7ee3347a718c8f56d3501976b986baa9c9a0b5f9
Santiago Rendón <52801967+Rendxn@users.noreply.github.com>
2022-07-19 14:51:01 -0500

    chore(test): better reports (#13)

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,4 +1,4 @@
-import {defineConfig} from "cypress";
+import { defineConfig } from "cypress";
 
 export default defineConfig({
   // setupNodeEvents can be defined in either
@@ -6,13 +6,21 @@ export default defineConfig({
   e2e: {
     video: false,
     screenshotOnRunFailure: false,
+    reporter: "cypress-multi-reporters",
+    reporterOptions: {
+      reporterEnabled: "mochawesome",
+      mochawesomeReporterOptions: {
+        reportDir: "cypress/reports/mocha",
+        quite: true,
+        overwrite: false,
+        html: false,
+        json: true,
+      },
+    },
     setupNodeEvents(on, config) {
       config.defaultCommandTimeout = 20000;
       config.responseTimeout = 20000;
-      // modify config values examples
-      // config.defaultCommandTimeout = 10000
 
-      // IMPORTANT return the updated config object
       return config;
     },
   },
